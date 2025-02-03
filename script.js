617b94730be82280d6218c7634bdbdddfa9eb681
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-02-03 18:01:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  
+  const textElement = document.getElementById("changing-text");
+const roles = ["Frontend Developer", "YouTuber", "Designer"];
+let index = 0;
+
+function changeText() {
+  textElement.style.opacity = 0; // Fade out the text
+  setTimeout(() => {
+    textElement.textContent = roles[index]; // Change the text
+    textElement.style.opacity = 1; // Fade in the new text
+    index = (index + 1) % roles.length; // Move to the next role in the array
+  }, 500); // Wait for the fade-out effect before changing the text
+}
+
+// Change text every 3 seconds
+setInterval(changeText, 3000);
